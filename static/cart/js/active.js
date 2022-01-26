7b170e887733c2ee84731cf11cdf9042e655fe64
r4mi4 <r4min.mh@gmail.com>
2022-01-26 16:20:26 +0330

    fixed some issues about Quantity Counter with using Nice Select library

---
+++ static/cart/js/active.js
@@ -0,0 +1,23 @@
+(function($) {
+    "use strict";
+     $(document).on('ready', function() {
+
+    /*==================================
+         Product page Quantity Counter
+     ===================================*/
+    $('.qty-box .quantity-right-plus').on('click', function () {
+        var $qty = $('.qty-box .input-number');
+        var currentVal = parseInt($qty.val(), 10);
+        if (!isNaN(currentVal)) {
+            $qty.val(currentVal + 1);
+        }
+    });
+    $('.qty-box .quantity-left-minus').on('click', function () {
+        var $qty = $('.qty-box .input-number');
+        var currentVal = parseInt($qty.val(), 10);
+        if (!isNaN(currentVal) && currentVal > 1) {
+            $qty.val(currentVal - 1);
+        }
+    });
+});
+})(jQuery);
