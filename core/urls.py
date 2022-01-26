8a8e420fa581d065c70995b931d03465e18350d4
r4mi4 <r4min.mh@gmail.com>
2022-01-26 12:04:30 +0330

    start shop app

--- core/urls.py
+++ core/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('company.urls', namespace='company')),
+    path('shop/', include('shop.urls', namespace='shop')),
 ]
