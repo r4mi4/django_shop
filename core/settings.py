e0992efe1f3d34e90604a8fd50dd446c2328fae3
r4mi4 <r4min.mh@gmail.com>
2022-01-28 17:45:15 +0330

    Identify Auth_User_model to Django

--- core/settings.py
+++ core/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Database
--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 class Category(models.Model):
