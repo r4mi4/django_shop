f890380dc472d0373a1dad884465de3d40acd2f6
r4mi4 <r4min.mh@gmail.com>
2022-02-01 10:49:19 +0330

    Add some extra field to orders model

--- orders/models.py
+++ orders/models.py
@@ -8,6 +8,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
+    billing_name = models.CharField(max_length=100, null=True, verbose_name='Name')
+    billing_address = models.CharField(max_length=255, null=True, verbose_name='Address')
+    billing_city = models.CharField(max_length=50, null=True, verbose_name='City')
+    billing_country = models.CharField(max_length=100, null=True, verbose_name='Country')
+    billing_post_code = models.CharField(max_length=30, null=True, verbose_name='Post Code')
+    stripe_id = models.CharField(max_length=50, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
