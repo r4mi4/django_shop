e298940b95a83ec9c1424b1976171316a790624e
r4mi4 <r4min.mh@gmail.com>
2022-02-01 11:15:54 +0330

    Improvement checkout.html

--- orders/forms.py
+++ orders/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+from .models import Order
+
 
 class CouponForm(forms.Form):
     code = forms.CharField()
+
+
+class OrderDetailsForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('billing_name', 'billing_address', 'billing_city', 'billing_country', 'billing_post_code')
