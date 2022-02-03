a89f45020c3410ea60cf6958039b9b01c280e65a
r4mi4 <r4min.mh@gmail.com>
2022-02-03 17:38:08 +0330

    add messages and complete user login section

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.user_logout, name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import  logout
+from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
+
+from .forms import UserLoginForm
 
 
-def login(request):
-    return render(request, 'accounts/login.html')
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, email=cd['email'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'success')
+                return redirect('company:home')
+            else:
+                messages.error(request, 'username or password is wrong !', 'danger')
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
 
 
 def register(request):
